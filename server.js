c74d64893b47df111769a1d37751de187bdabc8c
Tiago Gomes <tidgomes@gmail.com>
2023-03-13 11:32:39 -0300

    switch to event emitter

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const http = require('http')
 
 const port = 3000
 
-const app = http.createServer((req, res) => {
+const server = http.createServer()
+server.on('request', (req, res) => {
+  console.log('same thing as before')
   res.statusCode = 200
-  res.end(`<h1>Hello Tiago!</h1>`)
+  res.end('<h1>Hoi hoi</h1>')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`My app is runnig on port ${port}`)
 })
